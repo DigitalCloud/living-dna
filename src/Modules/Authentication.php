f2615b91684650ec30de1d38ded656e656412853
Yassir Awad <yassir.awad@dce.sa>
2019-08-19 13:49:43 +0300

    updates

--- src/Modules/Authentication.php
+++ src/Modules/Authentication.php
@@ -13,4 +13,13 @@ class Authentication extends Client
             'client_secret' => config("living-dna.client_secret")
         ]));
     }
+
+    public function refresh($refresh_token)
+    {
+        return self::send('post', 'api/refresh', json_encode([
+            'client_id' => config("living-dna.client_id"),
+            'client_secret' => config("living-dna.client_secret"),
+            'refresh_token' => $refresh_token
+        ]));
+    }
 }
